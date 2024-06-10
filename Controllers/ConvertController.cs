33cb4af35abd4ff42b1a5cfe02b5a2a4a3e86d84
Rajesh Rout <rajeshrauta5@gmail.com>
2024-06-11 00:37:56 +0530

    Add project files.

---
+++ Controllers/ConvertController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PdfToWordConverter.Services;
+
+namespace PdfToWordConverter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConvertController : ControllerBase
+    {
+        private readonly PdfToWordService _pdfToWordService;
+
+        public ConvertController(PdfToWordService pdfToWordService)
+        {
+            _pdfToWordService = pdfToWordService;
+        }
+
+        [HttpPost("pdf-to-word")]
+        public IActionResult ConvertPdfToWord(IFormFile pdfFile)
+        {
+            if (pdfFile == null || pdfFile.Length == 0)
+                return BadRequest("No file uploaded.");
+
+            var originalFileName = Path.GetFileNameWithoutExtension(pdfFile.FileName);
+            // Set the new file name with .docx extension
+            var newFileName = $"{originalFileName}.docx";
+
+            using (var memoryStream = new MemoryStream())
+            {
+                pdfFile.CopyTo(memoryStream);
+                var pdfBytes = memoryStream.ToArray();
+                var wordBytes = _pdfToWordService.ConvertPdfToWord(pdfBytes);
+                if (wordBytes == null)
+                    return StatusCode(500, "Conversion failed.");
+
+                return File(wordBytes, "application/vnd.openxmlformats-officedocument.wordprocessingml.document", newFileName);
+            }
+        }
+    }
+}
---
+++ Services/PdfToWordService.cs
@@ -0,0 +1,30 @@
+﻿using SautinSoft;
+using System.IO;
+
+namespace PdfToWordConverter.Services
+{
+    public class PdfToWordService
+    {
+        public byte[] ConvertPdfToWord(byte[] pdfBytes)
+        {
+            // Initialize PDF Focus object
+            PdfFocus pdfFocus = new PdfFocus();
+
+            // Set the input PDF byte array
+            pdfFocus.OpenPdf(pdfBytes);
+
+            if (pdfFocus.PageCount > 0)
+            {
+                // Convert PDF to Word in DOCX format
+                using (MemoryStream wordStream = new MemoryStream())
+                {
+                    pdfFocus.WordOptions.Format = PdfFocus.CWordOptions.eWordDocument.Docx;
+                    pdfFocus.ToWord(wordStream);
+                    return wordStream.ToArray();
+                }
+            }
+
+            return null;
+        }
+    }
+}
